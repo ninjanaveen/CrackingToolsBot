7876307c493b21d0e3581e1ef5ebb518b5ee2a62
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2021-01-04 18:03:12 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,15 @@
+import os
+
+
+class Config(object):
+    DL_LOCATION = "./starkgang/"
+    BOT_TOKEN = "xxxxx:xxxxxxxxx"
+    DEVS_EXPO = "DONTKANGTHISPLS"
+    DB_URI = "SoomnNibba"
+    API_HASH = "123456"
+    API_ID = "123456"
+    LOG_CHAT = "-10089468646"
+    OWNER_ID = "7777"
+    JOINCHANNEL_ENABLED = "True"
+    CHANNEL_ID = "-10029725234"
+    CHANNEL_LINK = "telegram.me/HeheFumny"
